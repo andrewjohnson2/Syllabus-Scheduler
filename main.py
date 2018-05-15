f225e159b78a34cd9a5c1a2f580f2805aff4ee58
ctarnopol <ctarn@umich.edu>
2018-05-15 12:13:06 -0400

    Added code for pdf scraper (not working)

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 
+import pyPdf
+pdf = pyPdf.PdfFileReader(open('test_pdf.pdf', "rb"))
+pdf_text = ''
+for page in pdf.pages:
+    pdf_text += page.extractText()
+
 
 
 import datefinder
 
 # open file name test_input.txt
 
-file = open('test_input.txt', 'r')
+#file = open('test_input.txt', 'r')
 
-text = file.read()
+#text = file.read()
 
-dates = datefinder.find_dates(text)
+dates = datefinder.find_dates(pdf_text)#text)
 
 for date in dates:
     print date
 
 
-file.close()
+#file.close()
