da22f6b9eaa2861074b2024f9e1e23e51f5fd68d
andrewjohnson2 <aj114114@gmail.com>
2018-05-15 15:44:27 -0700

    changes

--- main.py
+++ main.py
@@ -1,14 +1,59 @@
 import datefinder
 import pyPdf
-import parsedatetime
 
 
+def extract_date_strings(self, text, strict=False):
+    """
+    Scans text for possible datetime strings and extracts them
+
+    source: also return the original date string
+    index: also return the indices of the date string in the text
+    strict: Strict mode will only return dates sourced with day, month, and year
+    """
+    for match in self.DATE_REGEX.finditer(text):
+        match_str = match.group(0)
+        indices = match.span(0)
+
+        ## Get individual group matches
+        captures = match.capturesdict()
+        time = captures.get('time')
+        digits = captures.get('digits')
+        digits_modifiers = captures.get('digits_modifiers')
+        days = captures.get('days')
+        months = captures.get('months')
+        timezones = captures.get('timezones')
+        delimiters = captures.get('delimiters')
+        time = captures.get('time)')
+        time_periods = captures.get('time_periods')
+        extra_tokens = captures.get('extra_tokens')
+
+        if strict:
+            complete = False
+            ## 12-05-2015
+            if len(digits) == 3:
+                complete = True
+            ## 19 February 2013 year 09:10
+            elif (len(months) == 1) and (len(digits) == 1 or len(digits) == 2):
+                complete = True
+
+            if not complete:
+                continue
+
+        ## sanitize date string
+        ## replace unhelpful whitespace characters with single whitespace
+        match_str = re.sub('[\n\t\s\xa0]+', ' ', match_str)
+        match_str = match_str.strip(self.STRIP_CHARS)
+
+        ## Save sanitized source string
+        yield match_str, indices, captures
+
+
+datefinder.extract_date_strings = extract_date_strings
 
 pdf = pyPdf.PdfFileReader(open('Syllabus-ACC 301 VANCE.pdf', "rb"))
 pdf_text = ''
 for page in pdf.pages:
     pdf_text += page.extractText()
-
 
 # open file name test_input.txt
 
@@ -17,10 +62,9 @@ for page in pdf.pages:
 # text = file.read()
 
 # print pdf_text
-dates = datefinder.find_dates(pdf_text, False, True, False)  # text)
+dates = datefinder.find_dates(pdf_text, False, False, True)  # text)
 
-p = parsedatetime.Calendar();
-print p.parse("Monday, April 23, from 10:30 a.m. to 12:30 p.m.")
-
+for date in dates:
+    print date
 
 # file.close()
